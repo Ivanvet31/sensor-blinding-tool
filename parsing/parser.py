d4aadcec74b6294ddaaf8593df32a3c7f5353925
maxgum21 <maxgumyasko@gmail.com>
2024-07-01 14:07:02 +0300

    Parsing done

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+from os import replace
 import sys
 import random
 import re
@@ -12,40 +13,100 @@ def generate(len: int) -> str:
         res += random.choice(string.ascii_letters)
     return res
 
+def generate_match(opt_list) -> str:
+    packet_str = ""
+    for opt in opt_list:
+        pcre = opt.get("pcre", 0)
+        if pcre:
+            content = opt.get("content")
+            if len(content) > 300: continue
+            offset = int(opt.get("offset", 0))
+            depth = int(opt.get("depth", offset + len(content)))
+            length = int(opt.get("length", len(content)))
+
+            xeg = Xeger(limit=length)
+            reverse = ""
+            if opt.get("not"):
+                reverse = xeg.xeger(f"^(?!({content})).*")
+            else:
+                reverse = xeg.xeger(content)
+            left_buf = generate(max(0, offset - len(packet_str)))
+            right_buf = generate(max(0, depth - offset - len(content)))
+            packet_str = packet_str[:min(offset, len(packet_str))] + left_buf + reverse + right_buf + ("" if len(packet_str) <= depth else packet_str[depth:])
+        else:
+            content = opt.get("content")
+            content_str = ""
+            for i, chunk in enumerate(content.split('|')):
+                if i % 2:
+                    content_str += ''.join(list(map(lambda x: chr(int(x, 16)), chunk.split())))
+                else:
+                    content_str += chunk
+            offset = int(opt.get("offset", 0))
+            depth = int(opt.get("depth", offset + len(content_str)))
+            length = int(opt.get("length", len(content_str)))
+            left_buf = generate(max(0, offset - len(packet_str)))
+            right_buf = generate(max(0, depth - offset - len(content)))
+            packet_str = packet_str[:min(offset, len(packet_str))] + left_buf + content_str + right_buf + ("" if len(packet_str) <= depth else packet_str[depth:])
+    
+    return packet_str
+
+
 # TODO: wrtie http packet generation
-def generate_http(rules):
+def generate_http(rules) -> str:
     http_method = ""
     http_uri = ""
     http_header = ""
     http_client_body = ""
-    http_cookie = ""
+
+    uri_opts = list(filter(lambda x: bool(x.get("http_uri", 0)), rules))
+    method_opts = list(filter(lambda x: bool(x.get("http_method", 0)), rules))
+    header_opts = list(filter(lambda x: bool(x.get("http_header", 0)), rules))
+    client_body_opts = list(filter(lambda x: bool(x.get("http_client_body", 0)), rules))
+
     for rule in rules:
         if rule.get("http_uri", 0):
-            if rule.get("pcre"):
-                pass
-        elif rule.get("http_header", 0):
-            pass
+            uri_opts.append(rule)
         elif rule.get("http_method", 0):
-            pass
+            method_opts.append(rule)
+        elif rule.get("http_header", 0):
+            header_opts.append(rule)
         elif rule.get("http_client_body", 0):
-            pass
-        elif rule.get("http_cookie", 0):
-            pass
+            client_body_opts.append(rule)
+
+    http_uri = generate_match(uri_opts)
+    http_method = generate_match(method_opts)
+    http_header = generate_match(header_opts)
+    http_client_body = generate_match(client_body_opts)
+
+    if not http_uri:
+        http_uri = f"/{generate(3)}/{generate(5)}/{generate(4)}"
+    if not http_method:
+        http_method = "GET"
+    if not http_client_body:
+        http_client_body = generate(100)
+    if not http_header:
+        http_header = f"Host: localhost:8000\n"
+        for _ in range(10):
+            http_header += f"{generate(10)}: {generate(20)}\n"
+
+    http_header += f"Content-length: {len(http_client_body)}\n"
+    return f"{http_method} {http_uri} HTTP/1.1\n{http_header}\n{http_client_body}"
 
 packet_counter = 0
 packet_dir = "../packets/"
-packets = "packets_1.bin"
+#packets = "packets_1.bin"
+packets = "packets.bin"
 output = open(packet_dir + packets, "wb")
 
 def write_packet(protocol : int, port : int, packet : bytes):
     global packets, output, packet_counter
-    file_num = int(packets[8:-4])
-    if packet_counter > (file_num * 1000):
-        packets = f"packets_{file_num + 1}.bin"
-        output.close()
-        output = open(packet_dir + packets, "wb")
-        write_packet(protocol, port, packet)
-        return
+    #file_num = int(packets[8:-4])
+    #if packet_counter > (file_num * 1000):
+    #    packets = f"packets_{file_num + 1}.bin"
+    #    output.close()
+    #    output = open(packet_dir + packets, "wb")
+    #    write_packet(protocol, port, packet)
+    #    return
 
     if not output:
         return
@@ -53,13 +114,25 @@ def write_packet(protocol : int, port : int, packet : bytes):
     data = int.to_bytes(protocol) + int.to_bytes(port % 65536, 2) + int.to_bytes(len(packet), 4) + packet
 
     output.write(data)
+
+def colon_split(s):
+    ind = s.find(':')
+    if ind == -1: return [s]
+    return [ s[:ind], s[ind + 1:] ]
+
+def check_for_http(rule):
+    if rule.get("http_uri", 0): return True
+    if rule.get("http_method", 0): return True
+    if rule.get("http_header", 0): return True
+    if rule.get("http_client", 0): return True
+    return False
         
 if __name__ == "__main__":
     file_name = sys.argv[1]
     with open(file_name, "r") as rules:
         for line in rules.readlines():
-            if len(line) < 2: continue
             splat = line.split()
+            if len(line) < 2 or len(splat) < 7: continue
             protocol = splat[1]
 
             if splat[0] != "alert": 
@@ -68,7 +141,7 @@ if __name__ == "__main__":
             if splat[2] != "$EXTERNAL_NET":
                 continue
 
-            is_http = "HTTP" in line
+            is_http = False
             
             ports = ""
             match splat[6]:
@@ -83,8 +156,14 @@ if __name__ == "__main__":
                     ports = "[36,80,81,82,83,84,85,86,87,88,89,90,311,323,383,443,444,555,591,593,623,631,664,801,808,818,901,972,1158,1220,1270,1414,1533,1581,1719,1720,1741,1801,1812,1830,1942,2231,2301,2375,2381,2578,2809,2869,2980,3000,3029,3037,3057,3128,3323,3443,3702,4000,4343,4444,4592,4848,5000,5054,5060,5061,5117,5222,5250,5416,5443,5450,5480,5555,5600,5814,5894,5984,5985,5986,6060,6080,6173,6988,7000,7001,7005,7070,7071,7080,7144,7145,7180,7181,7510,7770,7777,7778,7779,8000,8001,8008,8014,8015,8020,8028,8040,8080,8081,8082,8085,8088,8090,8095,8118,8123,8161,8180,8181,8182,8222,8243,8280,8300,8333,8344,8393,8400,8443,8484,8500,8509,8511,8694,8787,8800,8848,8852,8880,8888,8899,8983,9000,9001,9002,9050,9060,9080,9090,9091,9111,9200,9201,9290,9443,9447,9502,9700,9710,9788,9830,9850,9990,9999,10000,10080,10100,10250,10255,10297,10443,11371,12601,13014,14592,15489,16000,16992,16993,16994,16995,17000,18081,19980,20000,29991,30007,30018,30888,33300,34412,34443,34444,36099,37215,40007,41080,44449,49152,49153,50000,50002,50452,51423,53331,54444,55252,55555,56712,110,143]"
                 case "$SIP_PORTS":
                     ports = "[5060,5061,5600]"
+                case "any":
+                    ports = str(random.randint(1000, 65535))
                 case _:
-                    ports = splat[6]
+                    if ports.isnumeric():
+                        ports = splat[6]
+                    else:
+                        continue
+
 
             opt_list = []
             option_string = line[line.find("(") + 1:line.rfind(")") - 1]
@@ -94,44 +173,49 @@ if __name__ == "__main__":
             for opt_line in option_list:
                 options_pcre = opt_line.split("pcre:")
 
-
-                option_pairs = list(map(lambda x: x.split(":"), options_pcre[0].split("; ")))
+                options_splat = options_pcre[0].split("; ")
+                option_pairs = list(map(colon_split, options_splat[1:]))
 
                 options = {}
                 if option_pairs[0][0] == '!':
-                    options['content'] = option_pairs[0][0][2:-1]
+                    options['content'] = options_splat[0][2:-1]
                     options['not'] = 1
                 else:
-                    options['content'] = option_pairs[0][0][1:-1]
+                    options['content'] = options_splat[0][1:-1]
                     options['not'] = 0
                 options["pcre"] = 0
 
                 for option in option_pairs[1::]:
                     if len(option) == 1:
+                        option[0] = option[0].replace("raw_", "")
                         options[option[0]] = 1
                         continue
                     options[option[0]] = option[1]
 
+                is_http = is_http or check_for_http(options)
                 opt_list.append(options)
 
                 for options_with_pcre in options_pcre[1:]:
 
-                    option_pairs = list(map(lambda x: x.split(":"), options_with_pcre.split("; ")))
+                    owp_splat = options_with_pcre.split("; ")
+                    option_pairs = list(map(colon_split, owp_splat[1:]))
 
                     options = {}
                     if option_pairs[0][0] == '!':
-                        options['content'] = option_pairs[0][0][2:-1]
+                        options['content'] = owp_splat[0][2:-1]
                         options['not'] = 1
                     else:
-                        options['content'] = option_pairs[0][0][1:-1]
+                        options['content'] = owp_splat[0][1:-1]
                         options['not'] = 0
                     options["pcre"] = 1
 
                     for option in option_pairs[1::]:
                         if len(option) == 1:
+                            option[0] = option[0].replace("raw_", "")
                             options[option[0]] = 1
                             continue
                         options[option[0]] = option[1]
+                    is_http = is_http or check_for_http(options)
 
                     opt_list.append(options)
 
@@ -141,47 +225,15 @@ if __name__ == "__main__":
 
             pcre = 0
 
+            packet_str = ""
             if is_http:
-                continue
+                packet_str = generate_http(opt_list)
             else:
-                packet_str = ""
-                for opt in opt_list:
-                    pcre = opt.get("pcre", 0)
-                    if pcre:
-                        content = opt.get("content")
-                        if len(content) > 300: continue
-                        offset = int(opt.get("offset", 0))
-                        depth = int(opt.get("depth", offset + len(content)))
-                        length = int(opt.get("length", len(content)))
+                packet_str = generate_match(opt_list)
 
-                        xeg = Xeger(limit=length)
-                        reverse = ""
-                        if opt.get("not"):
-                            reverse = xeg.xeger(f"^(?!({content})).*")
-                        else:
-                            reverse = xeg.xeger(content)
-                        left_buf = generate(max(0, offset - len(packet_str)))
-                        right_buf = generate(max(0, depth - offset - len(content)))
-                        packet_str = packet_str[:min(offset, len(packet_str))] + left_buf + reverse + right_buf + ("" if len(packet_str) <= depth else packet_str[depth:])
-                    else:
-                        content = opt.get("content")
-                        content_str = ""
-                        for i, chunk in enumerate(content.split('|')):
-                            if i % 2:
-                                content_str += ''.join(list(map(lambda x: chr(int(x, 16)), chunk.split())))
-                            else:
-                                content_str += chunk
-                        offset = int(opt.get("offset", 0))
-                        depth = int(opt.get("depth", offset + len(content_str)))
-                        length = int(opt.get("length", len(content_str)))
-                        left_buf = generate(max(0, offset - len(packet_str)))
-                        right_buf = generate(max(0, depth - offset - len(content)))
-                        packet_str = packet_str[:min(offset, len(packet_str))] + left_buf + content_str + right_buf + ("" if len(packet_str) <= depth else packet_str[depth:])
+            for ch in packet_str:
+                packet += int.to_bytes(ord(ch))
 
-
-                for ch in packet_str:
-                    packet += int.to_bytes(ord(ch))
-                        
 
             prot_id = 2
             match protocol:
